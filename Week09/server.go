97e65a83d62168d746a22ffa584c7f1d1896cf40
SenLinLeo <45943833+SenLinLeo@users.noreply.github.com>
2021-01-28 12:44:07 +0800

    [UPD]修改server

--- Week09/server.go
+++ Week09/server.go
@@ -1,34 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"time"
 )
 
-func main() {
-	listener, err := net.Listen("tcp", "localhost:8000")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Print(err)
-			continue
-		}
-		handleConn(conn)
-	}
-}
-
-func handleConn(c net.Conn) {
+func handleConn(c net.Conn, limitCh chan struct{}) {
+	limitCh <- struct{}{}
 	defer c.Close()
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		_, err := io.WriteString(c, time.Now().Format("2006-01-02 15:04:05\n")) // 循环 读写
 		if err != nil {
+			c.Close()
+			<-limitCh
+			fmt.Println("client主动断开连接...")
 			return
 		}
 		time.Sleep(1 * time.Second)
+		fmt.Println("当前开启协程数：", len(limitCh))
+	}
+	<-limitCh
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal("监听出错了： ", err)
+		return
+	}
+
+	limitChan := make(chan struct{}, 2)
+
+	for {
+		conn, err := listener.Accept() // 循环 listen
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		go handleConn(conn, limitChan)
 	}
 }
